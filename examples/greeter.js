9aff0c5cf9369b07b70c226dff30d7b1cba131f5
xuyq <569078987@qq.com>
2020-09-28 17:30:18 +0800

    update

--- examples/greeter.js
+++ examples/greeter.js
@@ -1,8 +1,27 @@
+var User = /** @class */ (function () {
+    function User(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = firstName + lastName;
+    }
+    return User;
+}());
 function greeter(person) {
     return 'Hello ' + person.firstName + '' + person.lastName;
 }
-var user = {
-    firstName: 'yq',
-    lastName: 'xu'
-};
+var user = new User('Qiang', 'Xu');
 console.log(greeter(user));
+var list = [1, 2, 3];
+var list2 = [1, 2, 3];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+var colorName = Color[2];
+console.log(colorName);
+var someValue = 'this is a string';
+var strLength = someValue.length;
+var strLength2 = someValue.length;
+console.log(strLength, strLength2);
--- examples/greeter.ts
+++ examples/greeter.ts
@@ -2,6 +2,7 @@ class User {
   fullName: string
   firstName: string
   lastName: string
+
   constructor(firstName: string, lastName: string) {
     this.firstName = firstName
     this.lastName = lastName
@@ -13,8 +14,26 @@ interface Person {
   firstName: string
   lastName: string
 }
+
 function greeter(person: Person) {
   return 'Hello ' + person.firstName + '' + person.lastName
 }
-let user = new User('Yee', 'Huang')
+
+let user = new User('Qiang', 'Xu')
 console.log(greeter(user))
+
+let list: number[] = [1, 2, 3]
+let list2: Array<number> = [1, 2, 3]
+
+enum Color {
+  Red = 1,
+  Green,
+  Blue
+}
+let colorName: string = Color[2]
+console.log(colorName)
+
+let someValue: any = 'this is a string'
+let strLength: number = (<string>someValue).length
+let strLength2: number = (someValue as string).length
+console.log(strLength, strLength2)
