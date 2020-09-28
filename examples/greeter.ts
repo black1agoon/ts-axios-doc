da67f119b0ded20bb344f69dbebd9fdfe3584fc7
xuyq <569078987@qq.com>
2020-09-28 16:27:04 +0800

    firstcommit

---
+++ examples/greeter.ts
@@ -0,0 +1,20 @@
+class User {
+  fullName: string
+  firstName: string
+  lastName: string
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.fullName = firstName + lastName
+  }
+}
+
+interface Person {
+  firstName: string
+  lastName: string
+}
+function greeter(person: Person) {
+  return 'Hello ' + person.firstName + '' + person.lastName
+}
+let user = new User('Yee', 'Huang')
+console.log(greeter(user))
